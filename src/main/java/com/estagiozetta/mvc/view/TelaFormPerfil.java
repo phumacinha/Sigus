2df36f65ffb351c0ec90931bfecb4c6b1d647419
Phumacinha <pedro.souza2@estudante.ufla.br>
2020-07-26 04:57:36 -0300

    tela formulário de cadastro/edição de perfil criada

---
+++ src/main/java/com/estagiozetta/mvc/view/TelaFormPerfil.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.estagiozetta.mvc.view;
+
+import javafx.scene.Node;
+
+/**
+ *
+ * @author pedro
+ */
+public class TelaFormPerfil extends TelaUtil {
+    public TelaFormPerfil() {
+        super("Adicionar perfil");
+    }
+
+    @Override
+    protected Node conteudoInicial() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
