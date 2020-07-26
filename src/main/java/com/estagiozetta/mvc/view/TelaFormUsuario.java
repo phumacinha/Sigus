830a06f5fea8804510b2c0a52d2bea79d263fc3f
Phumacinha <pedro.souza2@estudante.ufla.br>
2020-07-26 04:57:58 -0300

    tela formulário de cadastro/edição de usuario criada

---
+++ src/main/java/com/estagiozetta/mvc/view/TelaFormUsuario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.estagiozetta.mvc.view;
+
+import javafx.scene.Node;
+
+/**
+ *
+ * @author pedro
+ */
+public class TelaFormUsuario extends TelaUtil {
+    public TelaFormUsuario () {
+        super("Adcionar usuário");
+    }
+
+    @Override
+    protected Node conteudoInicial() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
